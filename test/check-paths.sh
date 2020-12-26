d4177185f0561e2d1a759374168cc6af1f9c9f36
Archibald125 <43479846+Archibald125@users.noreply.github.com>
2020-12-26 15:54:04 +0200

    Костыль в юнит-тестах

--- test/check-paths.sh
+++ test/check-paths.sh
@@ -49,15 +49,21 @@ while read -r file; do
 			if diff -d "$file" <(<"$file" iconv -c -f utf8 -t iso8859-5 2>/dev/null | tr -d $'\x7F-\x9F' | iconv -c -f iso8859-5 -t utf8 2>/dev/null); then
 				continue
 			else
-				echo "$file contains Unicode characters outside the ISO 8859-5 character set"
-				(( broken_files = broken_files + 1 ))
+				if diff -d "$file" <(<"$file" iconv -c -f utf8 -t iso8859-1 2>/dev/null | tr -d $'\x7F-\x9F' | iconv -c -f iso8859-1 -t utf8 2>/dev/null); then
+					continue
+				else
+					echo "$file contains Unicode characters outside the ISO 8859-5 and -1 character sets"
+					(( broken_files = broken_files + 1 ))
 			fi;;
 		*)
 			if diff -d "$file" <(<"$file" tr -d $'\x7F-\x9F' | iconv -c -f iso8859-5 -t utf8 2>/dev/null | iconv -c -f utf8 -t iso8859-5 2>/dev/null); then
 				continue
 			else
-				echo "$file contains characters outside the ISO 8859-5 character set"
-				(( broken_files = broken_files + 1 ))
+				if diff -d "$file" <(<"$file" tr -d $'\x7F-\x9F' | iconv -c -f iso8859-1 -t utf8 2>/dev/null | iconv -c -f utf8 -t iso8859-1 2>/dev/null); then
+					continue
+				else
+					echo "$file contains characters outside the ISO 8859-5 and -1 character sets"
+					(( broken_files = broken_files + 1 ))
 			fi;;
 	esac
 done < <(find . -name '*.dm')
