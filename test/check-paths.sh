19ace95798782612c2772f1e83a36c0796244692
Archibald125 <43479846+Archibald125@users.noreply.github.com>
2020-12-26 20:44:47 +0200

    Revert "Пробуем иначе"
    This reverts commit b97ce3a7f9a7d2574a104fb977c3dcfc5a9b9d2e.

--- test/check-paths.sh
+++ test/check-paths.sh
@@ -46,10 +46,10 @@ while read -r file; do
 		ASCII)
 			continue;;
 		UTF-8)
-			if diff -d "$file" <(<"$file" iconv -c -f utf8 -t iso8859-1 2>/dev/null | tr -d $'\x7F-\x9F' | iconv -c -f iso8859-1 -t utf8 2>/dev/null); then
+			if diff -d "$file" <(<"$file" iconv -c -f utf8 -t iso8859-5 2>/dev/null | tr -d $'\x7F-\x9F' | iconv -c -f iso8859-5 -t utf8 2>/dev/null); then
 				continue
 			else
-				if diff -d "$file" <(<"$file" iconv -c -f utf8 -t iso8859-5 2>/dev/null | tr -d $'\x7F-\x9F' | iconv -c -f iso8859-5 -t utf8 2>/dev/null); then
+				if diff -d "$file" <(<"$file" iconv -c -f utf8 -t iso8859-1 2>/dev/null | tr -d $'\x7F-\x9F' | iconv -c -f iso8859-1 -t utf8 2>/dev/null); then
 					continue
 				else
 					echo "$file contains Unicode characters outside the ISO 8859-5 and -1 character sets"
@@ -57,10 +57,10 @@ while read -r file; do
 				fi
 			fi;;
 		*)
-			if diff -d "$file" <(<"$file" tr -d $'\x7F-\x9F' | iconv -c -f iso8859-1 -t utf8 2>/dev/null | iconv -c -f utf8 -t iso8859-1 2>/dev/null); then
+			if diff -d "$file" <(<"$file" tr -d $'\x7F-\x9F' | iconv -c -f iso8859-5 -t utf8 2>/dev/null | iconv -c -f utf8 -t iso8859-5 2>/dev/null); then
 				continue
 			else
-				if diff -d "$file" <(<"$file" tr -d $'\x7F-\x9F' | iconv -c -f iso8859-5 -t utf8 2>/dev/null | iconv -c -f utf8 -t iso8859-5 2>/dev/null); then
+				if diff -d "$file" <(<"$file" tr -d $'\x7F-\x9F' | iconv -c -f iso8859-1 -t utf8 2>/dev/null | iconv -c -f utf8 -t iso8859-1 2>/dev/null); then
 					continue
 				else
 					echo "$file contains characters outside the ISO 8859-5 and -1 character sets"
